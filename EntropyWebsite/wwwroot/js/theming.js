500855bfa6d8816b008a5f4595c763a9b5785e91
Tewan <tewan@spycesoftware.com>
2021-03-22 22:01:53 +0100

    feat: Added draft of scss and test website

---
+++ EntropyWebsite/wwwroot/js/theming.js
@@ -0,0 +1,37 @@
+﻿let storedThemeId = localStorage.getItem("theme");
+
+let themeSelect = document.getElementById("theme-select");
+let body = document.querySelector("body");
+
+themeSelect.addEventListener("change", function () {
+    let option = themeSelect.selectedIndex;
+    
+    setTheme(option);
+});
+
+function setTheme(mode) {
+    switch (mode) {
+        case 0:
+            body.classList.add("theme-light");
+            body.classList.remove("theme-dark", "theme-system");
+        break;
+        case 1:
+            body.classList.add("theme-dark");
+            body.classList.remove("theme-light", "theme-system");
+        break;
+        case 2:
+            body.classList.add("theme-system");
+            body.classList.remove("theme-light", "theme-dark");
+        break;
+    }
+    
+    localStorage.setItem("theme", mode);
+}
+
+function initTheme() {
+    if(storedThemeId !== null) {
+        setTheme(storedThemeId);
+    } else {
+        setTheme(2);
+    }
+}
